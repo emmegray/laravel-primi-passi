f842b551b72ccb274a041358b6d6583d67757d7f
Manuela Ienuso <lunaienuso@libero.it>
2022-06-22 14:21:25 +0200

    Created base and added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="https://66.media.tumblr.com/1adfc2f48dd1760306566d238e33c172/tumblr_pllfwhE6ED1s9a9yjo1_400.gif" >
+    <h1 class="display-5 fw-bold">Hi there {{ $user }}!</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Epic phrase here</p>
+    </div>
+  </div>
+
+
+
+</body>
+</html>

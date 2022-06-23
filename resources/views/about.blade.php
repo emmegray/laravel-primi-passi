7a4fe33cce2ef5950d1e71e7c3e10c621d33aa31
Manuela Ienuso <lunaienuso@libero.it>
2022-06-23 10:33:51 +0200

    Added layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>About</title>
-</head>
-<body>
+@extends('layout.main')
 
-<div class="d-flex flex-wrap align-items-center bg-dark">
-        <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-          <img src="https://dbd.tricky.lol/dbdassets/perks/iconPerks_flipFlop.png" class="w-50">
-        </a>
-
-        <ul class="nav align-items-start">
-          <li><a href="/" class="nav-link link-light">Home</a></li>
-          <li><a href="/about" class="nav-link link-light">About</a></li>
-        </ul>
-</div>
-
+@section('about')
     <div class="px-4 py-5 my-5 row flex-lg-row-reverse align-items-center g-5">
       <div class="col-10 col-sm-8 col-lg-6">
         <img src="https://i.kym-cdn.com/photos/images/original/001/416/601/b46.gif" class="d-block mx-lg-auto img-fluid">
@@ -30,5 +11,4 @@
 
       </div>
     </div>
-</body>
-</html>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Home</title>
-</head>
-<body>
-<div class="d-flex flex-wrap align-items-center bg-dark">
-        <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-          <img src="https://dbd.tricky.lol/dbdassets/perks/iconPerks_flipFlop.png" class="w-50">
-        </a>
+@extends('layout.main')
 
-        <ul class="nav align-items-start">
-          <li><a href="/" class="nav-link link-light">Home</a></li>
-          <li><a href="/about" class="nav-link link-light">About</a></li>
-        </ul>
-</div>
-
+@section('home')
 <div class="px-4 py-5 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="https://66.media.tumblr.com/1adfc2f48dd1760306566d238e33c172/tumblr_pllfwhE6ED1s9a9yjo1_400.gif" >
     <h1 class="display-5 fw-bold">Hi there {{ $user }}!</h1>
@@ -33,8 +15,4 @@
       <a href="#" class="link-dark">{{ $email }}</a>
     </div>
   </div>
-
-
-
-</body>
-</html>
+@endsection
